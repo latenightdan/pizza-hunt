c50c5714b46c64cf228204f8489fce55677bb0b6
daniel natale <dnatale667@gmail.com>
2021-12-14 12:50:43 -0500

    used

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -51,6 +51,23 @@ const handlePizzaSubmit = event => {
   }
 
   const formData = { pizzaName, createdBy, size, toppings };
+
+  fetch('/api/pizzas', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => response.json())
+  .then(postRespone =>{
+    alert('PIZZA CREATED!!');
+    console.log(postRespone)
+  })
+  .catch(err => {
+    console.log(err);
+  })
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
